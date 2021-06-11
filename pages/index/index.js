022f3b08d2a39782c837ec0d493b3517176599fa
Mcguffen <504177380@qq.com>
2021-06-11 15:01:44 +0800

    调用接口

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,14 +4,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    // 发请求获取真实数据，行业动态列表
+    wx.request({
+      url: 'https://ku.qingnian8.com/school/list.php',
+      success:res => {
+        console.log(res);
+      }
+    })
   },
 
   /**
